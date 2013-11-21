b3e501242ffa1925d139103d92b7bb569caa102a
Feagurth <Feagurth@gmail.com>
2013-11-21 20:44:52 +0100

    Bienvenido 3

---
+++ Bienvenidos2/src/bienvenidos2/Bienvenidos2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bienvenidos2;
+
+/**
+ *
+ * @author Luis Cabrerizo Gómez
+ */
+public class Bienvenidos2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenido\na\nla\nprogramacion\nen\nJava!");
+    }
+    
+}
